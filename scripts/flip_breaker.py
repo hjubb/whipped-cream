ff5b377895daff00bc3117b054f19058d880e506
Harris <jubb@ibolpap.io>
2020-09-23 11:31:31 +1000

    assertion that account trying to flip breaker is owner in script

--- scripts/flip_breaker.py
+++ scripts/flip_breaker.py
@@ -7,4 +7,7 @@ load_dotenv(find_dotenv())
 def main():
     acct = accounts.at(os.getenv("DEPLOY_ADDRESS"))
     whipper = Whipper.at(os.getenv("CONTRACT_MAINNET_ADDRESS"))
+    print("whipper admin ", whipper.owner())
+    print("deploy account ", acct)
+    assert acct == whipper.owner()
     whipper.setBreaker(False, {'from': acct})
